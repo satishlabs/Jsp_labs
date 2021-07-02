e6995ab2d1148275454b8afc52b9027f564c5724
Satish Prasad <spd61185@gmail.com>
2021-07-02 09:16:49 +0530

    Lab11

--- Lab11/src/main/java/com/satishlabs/servlets/TestServlet.java
+++ Lab11/src/main/java/com/satishlabs/servlets/TestServlet.java
@@ -3,6 +3,7 @@ package com.satishlabs.servlets;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -11,6 +12,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.satishlabs.entity.Account;
+import com.satishlabs.entity.Address;
+import com.satishlabs.entity.Customer;
 
 /**
  * Servlet implementation class TestServlet
@@ -51,6 +56,37 @@ public class TestServlet extends HttpServlet {
 		al1.add(al);
 		al1.add(al);
 		request.setAttribute("AL1", al1);
+		
+		//4. Customer or User defined class object
+		Address add = new Address("Ranchi Main Road", "Ranchi");
+		Account acc = new Account(101, "SA", 9999.9, add);
+		Customer cust = new Customer(99,"satish", "satish@gmail", 9999, acc);
+		session.setAttribute("CUST", cust);
+		
+		//5. Collection of User defined class object
+		ArrayList<Customer> cs = new ArrayList<Customer>();
+		cs.add(cust);
+		cs.add(cust);
+		cs.add(cust);
+		cs.add(cust);
+		cs.add(cust);
+		session.setAttribute("CS", cs);
+		
+		//6. Map Object
+		HashMap<String, String> hm = new HashMap<String, String>();
+		hm.put("sid", "11");
+		hm.put("sname", "Satish");
+		hm.put("email", "satish@gmail");
+		session.setAttribute("HM", hm);
+		
+		//7. Collection of Map object (*JSTL)
+		ArrayList<HashMap<String, String>> al2 = new ArrayList<HashMap<String,String>>();
+		al2.add(hm);
+		al2.add(hm);
+		al2.add(hm);
+		request.setAttribute("AL2", al2);
+		RequestDispatcher rd1 = request.getRequestDispatcher("show.jsp");
+		rd1.forward(request, response);
 	}
 
 }
--- Lab11/src/main/webapp/show.jsp
+++ Lab11/src/main/webapp/show.jsp
@@ -29,7 +29,7 @@
 				</td>
 		</tr>
 		<tr>
-			<td>2. Collection of Collections.</td>
+			<td>3. Collection of Collections.</td>
 			
 			<td>Coming Soon</td>
 		</tr>
